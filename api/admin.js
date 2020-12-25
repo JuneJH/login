1982ba454a6368672b8aaaa6fc816572a3af594b
JuneJH <15310487388@163.con>
2020-12-25 22:35:39 +0800

    调整

--- api/admin.js
+++ api/admin.js
@@ -1,51 +1,51 @@
 const express = require('express');
 const {pickProps} = require("../util/tool");
 const {publish} = require("../util/jwt");
-const {errRes,succeed} = require("../util/response");
+const {errRes, succeed} = require("../util/response");
 const multer = require("multer");
-const path =require("path");
+const path = require("path");
 const router = express.Router();
-const {addAdmin,login,updateAdmin,getAdminByUsername,getAdminById} = require("../services/adminService");
+const {addAdmin, login, updateAdmin, getAdminByUsername, getAdminById} = require("../services/adminService");
 // 注册
-router.post("/register",async (req,res)=>{
+router.post("/register", async (req, res) => {
     const results = await addAdmin(req.body);
-    if(results.isOk){
-        const user = pickProps(results.succeed.toJSON(),"id","username","mobile","email","count")
-        succeed(res,200,"注册成功",user)
-    }else{
-        errRes(res,400,"参数验证不通过",results.err)
+    if (results.isOk) {
+        const user = pickProps(results.succeed.toJSON(), "id", "username", "mobile", "email", "count")
+        succeed(res, 200, "注册成功", user)
+    } else {
+        errRes(res, 400, "参数验证不通过", results.err)
     }
 });
 // 更改个人信息
-router.patch("/update",async (req,res)=>{
-    let results = await updateAdmin(req.user.id,req.body);
-    if(results.isOk){
-        let newAdmin =await getAdminById(req.user.id);
-        newAdmin = pickProps(newAdmin,"id","username","mobile","email","count","headerImg")
-        const  token = publish(3600*7,newAdmin);
-        res.header("authorization",token);
-        succeed(res,200,"修改成功",newAdmin);
-    }else{
-        errRes(res,400,"参数验证不通过",results.err)
+router.patch("/update", async (req, res) => {
+    let results = await updateAdmin(req.user.id, req.body);
+    if (results.isOk) {
+        let newAdmin = await getAdminById(req.user.id);
+        newAdmin = pickProps(newAdmin, "id", "username", "mobile", "email", "count", "headerImg")
+        const token = publish(3600 * 7, newAdmin);
+        res.header("authorization", token);
+        succeed(res, 200, "修改成功", newAdmin);
+    } else {
+        errRes(res, 400, "参数验证不通过", results.err)
     }
 })
-router.post("/login",async (req,res)=>{
-    let results = await login(req.body.username,req.body.password);
-    if(results){
+router.post("/login", async (req, res) => {
+    let results = await login(req.body.username, req.body.password);
+    if (results) {
         results = results.toJSON();
-        results = pickProps(results,"id","username","mobile","email","count","headerImg")
-        await updateAdmin(results.id,{count:results.count + 1})
-        const  token = publish(3600*7,results);
-        res.header("authorization",token);
-        succeed(res,200,"登录成功",results);
-    }else{
-        errRes(res,400,"用户名和密码不匹配",{})
+        results = pickProps(results, "id", "username", "mobile", "email", "count", "headerImg")
+        await updateAdmin(results.id, {count: results.count + 1})
+        const token = publish(3600 * 7, results);
+        res.header("authorization", token);
+        succeed(res, 200, "登录成功", results);
+    } else {
+        errRes(res, 400, "用户名和密码不匹配", {})
     }
 })
 router.get("/whoIam", async (req, res) => {
     let results = await getAdminByUsername(req.user.username);
     if (results) {
-        results = pickProps(results, "username", "mobile", "email", "count","headerImg");
+        results = pickProps(results, "username", "mobile", "email", "count", "headerImg");
         succeed(res, 200, "获取成功", results)
     } else {
         errRes(res, 500, "目前没有相关信息", "获取失败")
@@ -60,17 +60,20 @@ const storage = multer.diskStorage({
     },
     filename: function (req, file, cb) {
         const suffix = path.extname(file.originalname);
-        const fileName =Date.now() + Math.random(36).toString().slice(-6) + suffix;
+        const fileName = Date.now() + Math.random(36).toString().slice(-6) + suffix;
         cb(null, fileName)
     }
 })
-router.post("/uploadHeaderImg",multer({storage,limits:{fileSize: 700 * 1024}}).single("headerImg"),async (req,res)=>{
+router.post("/uploadHeaderImg", multer({
+    storage,
+    limits: {fileSize: 700 * 1024}
+}).single("headerImg"), async (req, res) => {
     const headerPath = path.join("/headerImg", req.file.filename);
-    const result = await updateAdmin(req.user.id,{headerImg:headerPath})
-    if(result.isOk){
-        succeed(res,200,"修改成功","成功修改头像")
-    }else{
-        errRes(res,500,"服务器已收到你的请求","正在处理中")
+    const result = await updateAdmin(req.user.id, {headerImg: headerPath})
+    if (result.isOk) {
+        succeed(res, 200, "修改成功", "成功修改头像")
+    } else {
+        errRes(res, 500, "服务器已收到你的请求", "正在处理中")
     }
 })
 
--- services/adminService.js
+++ services/adminService.js
@@ -2,141 +2,142 @@ const md5 = require("md5");
 const Admin = require("../models/Admin");
 const validate = require("validate.js");
 //检查username是否存在
-validate.validators.isExistUserName = async (value)=>{
-    if(!value)return ;
-    const result =  await getAdminByUsername(value);
-    if(result){
+validate.validators.isExistUserName = async (value) => {
+    if (!value) return;
+    const result = await getAdminByUsername(value);
+    if (result) {
         return "^%{value}已存在,请选择专属您的用户名"
-    };
+    }
+    ;
 }
 // 检查密码是否符合规定
-validate.validators.checkPassword = async (value)=>{
-    if(!value)return ;
+validate.validators.checkPassword = async (value) => {
+    if (!value) return;
     const reg = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,}$/
-    if(!reg.test(value)){
+    if (!reg.test(value)) {
         return "^您的密码格式在目前这个危险的世界并不安全"
-    };
+    }
 }
-getAdminByUsername = async function(username) {
+getAdminByUsername = async function (username) {
     const result = await Admin.findOne({where: {username}});
     return result ? result.toJSON() : null;
 }
 exports.getAdminByUsername = getAdminByUsername;
-exports.addAdmin =async function(obj){
+exports.addAdmin = async function (obj) {
     // 验证是否为标准的格式
     const rules = {
-        username:{
-            presence:{
-                allowEmpty:false,
-                message:"^伟人的思想总是有着惊人的相似,但也有着许多的不同"
-            },
-            type:"string",
-            length:{
-                minimum:1,
-                maximum:100,
-                message: "^用户名的长度必须在1~100之间"
-            },
-            isExistUserName:true,
-        },
-        password:{
+        username: {
             presence: {
                 allowEmpty: false,
-                message:"^好好想想一个只有你知道的密码"
+                message: "^伟人的思想总是有着惊人的相似,但也有着许多的不同"
             },
-            type:"string",
+            type: "string",
+            length: {
+                minimum: 1,
+                maximum: 100,
+                message: "^用户名的长度必须在1~100之间"
+            },
+            isExistUserName: true,
+        },
+        password: {
+            presence: {
+                allowEmpty: false,
+                message: "^好好想想一个只有你知道的密码"
+            },
+            type: "string",
             length: {
                 minimum: 6,
                 maximum: 16,
                 message: "^密码的长度必须在6~16之间"
             },
-            checkPassword:true
+            checkPassword: true
         },
-        mobile:{
-            type:"string",
-            length:{
-                is:11,
+        mobile: {
+            type: "string",
+            length: {
+                is: 11,
                 message: "^您的移动电话需要11位"
             }
         },
-        email:{
-            type:"string",
+        email: {
+            type: "string",
             email: {
-                message:"^邮箱不是这样子滴"
+                message: "^邮箱不是这样子滴"
             },
         },
-        count:{
-            presence:{
-                allowEmpty:false,
+        count: {
+            presence: {
+                allowEmpty: false,
             },
-            type:"number",
+            type: "number",
         },
     }
     obj.count = 0;
-    try{
-        const result = await validate.async(obj,rules)
+    try {
+        const result = await validate.async(obj, rules)
         obj.password = md5(obj.password);
         const succeed = await Admin.create(obj);
-        return {isOk:true,succeed};
-    }catch (err){
-        return {isOk:false,err};
+        return {isOk: true, succeed};
+    } catch (err) {
+        return {isOk: false, err};
     }
 }
-exports.updateAdmin =async function(id,obj){
+exports.updateAdmin = async function (id, obj) {
     // 验证是否为标准的格式
     const rules = {
-        username:{
-            type:"string",
-            length:{
-                minimum:1,
-                maximum:100,
+        username: {
+            type: "string",
+            length: {
+                minimum: 1,
+                maximum: 100,
             },
-            isExistUserName:true,
+            isExistUserName: true,
         },
-        password:{
-            type:"string",
+        password: {
+            type: "string",
             length: {
                 minimum: 6,
                 maximum: 16,
             },
-            checkPassword:true
+            checkPassword: true
         },
-        mobile:{
-            type:"string",
-            length:{
-                is:11,
+        mobile: {
+            type: "string",
+            length: {
+                is: 11,
             }
         },
-        email:{
-            type:"string",
+        email: {
+            type: "string",
             email: true,
         },
-        headerImg:{
-            type:"string",
+        headerImg: {
+            type: "string",
         },
-        count:{
-            type:"number",
+        count: {
+            type: "number",
         },
     }
-    try{
-        const result = await validate.async(obj,rules)
-        if(obj.password){
+    try {
+        const result = await validate.async(obj, rules)
+        if (obj.password) {
             obj.password = md5(obj.password);
         }
-        const succeed = await Admin.update(obj,{
-            where:{
+        const succeed = await Admin.update(obj, {
+            where: {
                 id
             }
         });
-        return {isOk:true,succeed};
-    }catch (err){
-        return {isOk:false,err};
+        return {isOk: true, succeed};
+    } catch (err) {
+        return {isOk: false, err};
     }
 }
-exports.getAdminById = async function(id) {
+exports.getAdminById = async function (id) {
     const result = await Admin.findOne({where: {id}});
     return result ? result.toJSON() : null;
 }
-exports.login = async function(username,password){
+exports.login = async function (username, password) {
     password = md5(password);
-    return await Admin.findOne({where:{username,password}})
+    return await Admin.findOne({where: {username, password}})
 }
